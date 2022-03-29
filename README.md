b98425ebf5846677e3b1e69275a1cfe98450e42c
Mia Borja <apk.borja@gmail.com>
2022-03-29 11:34:02 -0700

    Added Geocoding API to retrieve latitude/longitude.

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ TBA
 
 ## Tools
 
-TBA
+* OpenWeather API
+* Bootstrap 5.8.1
 
 ## Screenshots
 
---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var searchForm = document.querySelector("#city-form");
+var forecastInfo = document.querySelector("#forecast-info");
+var searchHistory = document.querySelector("#search-history");
+var cityInput = document.querySelector("#city-search")
+
+var submitCity = function (event) {
+    event.preventDefault();
+
+    var city = cityInput.value.trim();
+
+    if (city) {
+        globalSearch(city);
+    }
+}
+
+
+var globalSearch = function (city) {
+    var geoCodeApi = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=4956f3dda9aec433400a8f2f8bb5d4cd'
+
+    fetch(geoCodeApi)
+    .then (function (response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        } else {
+            alert("Please enter a real city name.")
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect.")
+    });
+};
+
+var weatherSearch = function (lat, lon) {
+    var weatherApi = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appid=4956f3dda9aec433400a8f2f8bb5d4cd'
+}
+
+var todaysForecast = function() {
+
+}
+
+function saveSearch() {
+    localStorage.setItem();
+}
+
+
+searchForm.addEventListener("submit", submitCity);
+//searchHistory.addEventListener("click");
