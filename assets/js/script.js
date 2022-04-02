f1e278e5ffcd4adb79def78325a89d71726e3a90
Mia Borja <apk.borja@gmail.com>
2022-04-01 22:39:01 -0700

    Created html basis for cards.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ var submitCity = function (event) {
 
     if (city) {
         globalSearch(city);
+        saveSearch(city);
 
     } else {
         alert("Please enter a city.")
@@ -55,7 +56,10 @@ var weatherSearch = function (lat, lon) {
     })
 };
 
-var todaysForecast = function(current) {
+var todaysForecast = function(data) {
+
+    document.querySelector("#weather-info").classList.remove("hidden");
+
     var cityHeading = document.querySelector("#city-name");
     var temperature = document.querySelector("#weather-temp");
     var wind = document.querySelector("#weather-wind");
@@ -65,21 +69,65 @@ var todaysForecast = function(current) {
 
     cityHeading.textContent = cityInput.value.trim();
 
-    document.querySelector("#weather-info").setAttribute("display","block")
+    temperature.textContent = "Temp: " + data.current.temp + "°F";
+    wind.textContent = "Wind: " + data.current.wind_speed + "MPH";
+    humidity.textContent = "Humidity: " + data.current.humidity + "%";
+    weatherIcon.src = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png" ;
+    uvIndex.textContent = "UV Index: " + data.current.uvi;
+    cityHeading.textContent = cityInput.value;
 
-        temperature.textContent = "Temp: " + current.temp + "°F";
-        wind.textContent = "Wind: " + current.wind_speed + "MPH";
-        humidity.textContent = "Humidity: " + current.humidity + "&deg;";
-        weatherIcon.src = "http://openweathermap.org/img/wn/" + current.weather.icon ;
-        uvIndex.textContent = "UV Index: " + current.uvi;
-        cityName.textContent = cityInput.value;
+    if (data.current.uvi < 3) {
+        uvIndex.setAttribute("style", "background: green;")
+    } else if (data.current.uvi > 3 && data.current.uvi < 6) {
+        uvIndex.setAttribute("style", "background: yellow;")
+    } else {
+        uvIndex.setAttribute("style", "background: red;")
+    }
 };
 
+var fiveForecast = function(data) {
+    const popCard = document.createElement("div");
+    const popImage = document.createElement("img");
+    const popUL = document.createElement("ul");
+    const popTemp = document.createElement("li");
+    const popWind = document.createElement("li");
+    const popHumid = document.createElement("li");
 
-function saveSearch() {
-    localStorage.setItem();
+    //for (var i = 0; i < data.daily.length; i++) {
+
+    //}
+
+    temperature.textContent = "Temp: " + data.daily[i].temp + "°F";
+    wind.textContent = "Wind: " + data.daily[i].wind_speed + "MPH";
+    humidity.textContent = "Humidity: " + data.daily[i].humidity + "%";
+    weatherIcon.src = "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png" ;
 }
 
 
+function saveSearch(city) {
+
+    var getLocation = document.createElement("button");
+    getLocation.classList.add("city-button");
+    getLocation.textContent = city;
+
+    searchHistory.appendChild(getLocation);
+
+    localStorage.setItem("location", JSON.stringify(city));
+
+}
+
+function pullSearch() {
+    JSON.parse(localStorage.getItem("location"));
+}
+
+var buttonSearch = function(event) {
+    event.preventDefault();
+    var city = this.innerText;
+
+    globalSearch(city);
+}
+
+pullSearch();
+
 searchForm.addEventListener("submit", submitCity);
-//searchHistory.addEventListener("click");
+searchHistory.addEventListener("click", buttonSearch);
